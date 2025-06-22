c999ce75d08187b98b40e8f5d3d9a36ad38abdd8
Jabotime101 <Jabolearns@gmail.com>
2025-06-22 11:54:42 -0400

    feat: refactor auth with NextAuth and fix build errors

--- next.config.js
+++ next.config.js
@@ -2,18 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['fluent-ffmpeg'],
-  },
   reactStrictMode: true,
   env: {
     // Make sure these are set in your .env.local file
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   },
-  // Set the default port for the development server
-  devServer: {
-    port: 3000,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
